2c4806339b7d2edcacaa2a135cd98923aeb64877
Jen Kang <kang0552@umn.edu>
2024-04-09 12:06:36 -0500

    change home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
   let navigate = useNavigate();
 
+  useEffect(() => {
+    // 假设 fetchUser是一个函数，用于检查用户的登录状态
+    // 这可能需要您通过调用特定的API来实现
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('/.auth/me');
+        const data = await response.json();
+        if (data.clientPrincipal) {
+          // 如果用户已经登录，重定向到/todos页面
+          navigate('/todos');
+        }
+      } catch (error) {
+        console.error('Error fetching user', error);
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
   const handleLogin = () => {
-    // 使用window.location.origin获取当前页面的基础URL，然后拼接认证路径
     const authUrl = `${window.location.origin}/.auth/login/github`;
     window.location.href = authUrl;
   };
